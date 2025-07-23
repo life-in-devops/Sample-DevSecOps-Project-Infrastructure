33721195ed281c2ec864c990adc83769041237fd
life-in-devops <lifeindevops@gmail.com>
2025-07-23 14:23:05 +0530

    Update: deployment script for deleting the infra

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -35,7 +35,7 @@ resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
 
 #cluster provision
 resource "aws_eks_cluster" "eks_cluster" {
-  name     = "Nginx-Ingress-Project"
+  name     = "Sample-DevSecOps-Project"
   role_arn = aws_iam_role.eks_role.arn
 
   vpc_config {
@@ -83,9 +83,10 @@ resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
 #create node group
 resource "aws_eks_node_group" "eks_node_group" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
-  node_group_name = "Nginx-Ingress-Project-Node-Group"
+  node_group_name = "Sample-DevSecOps-Project-Node-Group"
   node_role_arn   = aws_iam_role.eks_node_role.arn
   subnet_ids      = var.private_subnet_ids
+
 
   scaling_config {
     desired_size = 1
